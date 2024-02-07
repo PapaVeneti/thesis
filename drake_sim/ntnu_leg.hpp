c31a19758896b5eb2000f9fb409ec09baea5654b
PapaVeneti <michael.d.papadakis@gmail.com>
2024-02-07 18:33:16 +0200

    first_instance_that_compiles

--- drake_sim/ntnu_leg.hpp
+++ drake_sim/ntnu_leg.hpp
@@ -14,10 +14,12 @@
 
 
 using meshcat_shared_ptr = std::shared_ptr<drake::geometry::Meshcat>; 
-using drake_tfd = drake::math::RigidTransform<double>;
-using drake_rotMat = drake::math::RotationMatrix<double>;
+
+using drake_tfd       = drake::math::RigidTransform<double>;
+using drake_rotMat    = drake::math::RotationMatrix<double>;
 using drake_rigidBody = drake::multibody::RigidBody<double> ; 
-using drake_plant = drake::multibody::MultibodyPlant<double>;
+using drake_plant     = drake::multibody::MultibodyPlant<double>;
+using drake_builder   = drake::systems::DiagramBuilder<double>;
 
 #define mb_time_step 0.0002 //in seconds!!!! -> must be as small as the simulation
 
@@ -27,7 +29,12 @@ class ntnu_leg
 private:
   /* data */
 public:
-  ntnu_leg(const bool right_side = true, const drake_rigidBody & ParentBody , const drake_tfd & TF_B_MH);
+  ntnu_leg(
+    drake_builder & builder,
+    drake_plant & plant,
+    const bool right_side, 
+    const drake_rigidBody & ParentBody, 
+    const drake_tfd & TF_B_MH);
   ~ntnu_leg();
 };
 
